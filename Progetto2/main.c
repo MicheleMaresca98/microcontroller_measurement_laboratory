613917dfcb1ae5d4f5b278674216947932f72b02
Michele Maresca <58850779+MicheleMaresca98@users.noreply.github.com>
2021-02-15 15:20:27 +0100

    First commit

---
+++ Progetto2/main.c
@@ -0,0 +1,7 @@
+#include <stm32f30x.h>
+int main()
+{RCC->AHBENR |= RCC_AHBENR_GPIOEEN;
+GPIOE->MODER |=GPIO_MODER_MODER9_0 | GPIO_MODER_MODER8_0;
+GPIOE->ODR |= GPIO_ODR_9 | GPIO_ODR_8;
+while(1);
+}
